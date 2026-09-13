9b75785619d3868ed8d9f2b6a8470dd73a5e3548
cooki7101-pixel <cooki7101@gmail.com>
2026-09-13 22:04:01 +0900

    about추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ProjectPassing from "./pages/projects/ProjectPassing";
+import About from "./pages/about/About";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/works/project-passing" element={<ProjectPassing />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
